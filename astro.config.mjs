856dd11150787e40205024ef8a30d5bd9cced038
afer0093 <peanuts1909@gmail.com>
2024-01-03 03:53:59 +0900

    modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	new file:   src/components/BlogPost.astro 	new file:   src/components/Footer.astro 	new file:   src/components/Greeting.jsx 	new file:   src/components/Hamburger.astro 	new file:   src/components/Header.astro 	new file:   src/components/Navigation.astro 	new file:   src/components/Social.astro 	new file:   src/components/ThemeIcon.astro 	new file:   src/layouts/BaseLayout.astro 	new file:   src/layouts/MarkdownPostLayout.astro 	modified:   src/pages/about.astro 	modified:   src/pages/blog.astro 	modified:   src/pages/index.astro 	modified:   src/pages/posts/post-1.md 	modified:   src/pages/posts/post-2.md 	modified:   src/pages/posts/post-3.md 	new file:   src/pages/posts/post-4.md 	new file:   src/pages/rss.xml.js 	new file:   src/pages/tags/[tag].astro 	new file:   src/pages/tags/index.astro 	new file:   src/scripts/menu.js 	new file:   src/styles/global.css 	modified:   tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://astro-tutorial.github.io'
+  site: 'https://astro-tutorial.github.io',
+  integrations: [preact()]
 });
